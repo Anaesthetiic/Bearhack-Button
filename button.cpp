f50509ccf84e63089ee261f36b518dd6d49073dc
Anaesthetiic <tyoniclebionicle@gmail.com>
2025-04-05 16:56:43 -0700

    Revert "Added feature"
    This reverts commit fb8c32d11abae66d0cd81c36c80a07f42136c512.

--- button.cpp
+++ button.cpp
@@ -40,8 +40,6 @@ void loop() {
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
 
-delay(50msssss)
-
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
     // turn LED on:
